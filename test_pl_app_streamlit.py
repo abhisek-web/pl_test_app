c69364f933c59206ec511368fe8a62e453aeaf30
Abhisek De <deabhisek2@gmail.com>
2024-07-11 22:22:10 +0530

    Add files via upload

---
+++ test_pl_app_streamlit.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import requests
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+def get_players_data():
+    # Get general information
+    response = requests.get('https://fantasy.premierleague.com/api/bootstrap-static/')
+    data = response.json()
+
+    # Convert players data to DataFrame
+    players_df = pd.DataFrame(data['elements'])
+
+    return players_df
+
+if st.button('Get Players Data'):
+    players_df = get_players_data()
+    st.write(players_df)
